95bcae868e768d884123d337d39885b45e7763fb
pkl2025 <pkl2025uii@gmail.com>
2025-05-02 13:44:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # https://drive.google.com/file/d/1YNxu_bLeftHjwAJGbczNZ6HGcRVqV8FQ/view?usp=sharing
 # https://drive.google.com/file/d/1cT0Ndwustu_g0eIlW0IPrIkGoeIWYO7M/view?usp=sharing
+
 import streamlit as st
 
-st.set_page_config(page_title="Google Drive PDF Viewer", layout="wide")
-st.title("📄 PDF Viewer dari Google Drive")
+st.set_page_config(page_title="PDF Viewer", layout="wide")
+st.title("📄 PDF Viewer")
 
 # Daftar file PDF dengan nama tampilannya dan ID Google Drive-nya
 pdf_files = {
-    "Modul 1": "1YNxu_bLeftHjwAJGbczNZ6HGcRVqV8FQ",
-    "Modul 2": "1cT0Ndwustu_g0eIlW0IPrIkGoeIWYO7M",
+    "AgendaPKL": "1YNxu_bLeftHjwAJGbczNZ6HGcRVqV8FQ",
+    "TestPCCR": "1cT0Ndwustu_g0eIlW0IPrIkGoeIWYO7M",
 }
 
 # Pilih file dari dropdown
@@ -18,12 +19,13 @@ selected_file_id = pdf_files[selected_title]
 # URL embed Google Drive
 embed_url = f"https://drive.google.com/file/d/{selected_file_id}/preview"
 
-# Tampilkan di iframe
+# Tampilkan di iframe yang dipusatkan
 st.subheader(f"📖 Menampilkan: {selected_title}")
 st.components.v1.html(
     f"""
-    <iframe src="{embed_url}" width="100%" height="800px" style="border:none;"></iframe>
+    <div style="display: flex; justify-content: center;">
+        <iframe src="{embed_url}" width="80%" height="800px" style="border:none;"></iframe>
+    </div>
     """,
     height=800,
 )
-
